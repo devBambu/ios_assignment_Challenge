957453e91483c19fc6ca8c53a932e7c1a8691924
Bambu <byrr113@gmail.com>
2026-03-17 22:32:40 +0900

    Revert "✨ Feat: 검색 기능 구현 (UI 제외)"

--- Assignment_Challenge/Assignment_Challenge/Model/MusicResponse.swift
+++ Assignment_Challenge/Assignment_Challenge/Model/MusicResponse.swift
@@ -9,13 +9,3 @@ nonisolated
 struct MusicResponse: Codable {
     let results: [Music]
 }
-
-nonisolated
-struct TvShowResponse: Codable {
-    let results: [TvShow]
-}
-
-nonisolated
-struct PodcastResponse: Codable {
-    let results: [Podcast]
-}
