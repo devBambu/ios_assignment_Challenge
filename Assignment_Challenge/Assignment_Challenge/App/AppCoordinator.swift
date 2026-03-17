e929140c73d39290814d03dc7ebfec96f7bb0e21
t2025-m0143 <>
2026-03-17 22:30:38 +0900

    Revert "✨feat: 중복 구독 방지를 위한 share() 추가"
    This reverts commit 4733a7705b217468153b5817c64c6bf4065e4d54.

--- Assignment_Challenge/Assignment_Challenge/App/AppCoordinator.swift
+++ Assignment_Challenge/Assignment_Challenge/App/AppCoordinator.swift
@@ -40,10 +40,7 @@ class AppCoordinator: Coordinator {
     }
     
     func setupSearchController(for vc: HomeViewController) {
-        let resultVC = ResultViewController(
-            viewModel: HomeViewModel(networkService: networkService),
-            searchKeyword: vc.searchKeywordRelay.asObservable()
-        )
+        let resultVC = ResultViewController(viewModel: HomeViewModel(networkService: networkService), searchKeyword: vc.searchKeywordRelay.asObservable())
         let searchController = UISearchController(searchResultsController: resultVC)
         
         searchController.obscuresBackgroundDuringPresentation = false // 검색바 클릭시 반투명하게 보이기
