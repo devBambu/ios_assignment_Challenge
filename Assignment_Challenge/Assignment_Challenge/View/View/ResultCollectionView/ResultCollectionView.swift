2735317b3c7180ed72e710ee5ccc116ded3514ba
t2025-m0143 <>
2026-03-18 12:41:53 +0900

    ✨feat: ResultCollectionView Layout

--- Assignment_Challenge/Assignment_Challenge/View/View/ResultCollectionView/ResultCollectionView.swift
+++ Assignment_Challenge/Assignment_Challenge/View/View/ResultCollectionView/ResultCollectionView.swift
@@ -5,3 +5,47 @@
 //  Created by t2025-m0143 on 3/18/26.
 //
 
+import UIKit
+
+final class ResultCollectionView: UICollectionView {
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: UICollectionViewLayout())
+        collectionViewLayout = makeCompositionalLayout()
+        layoutMargins = .init(top: 0, left: 20, bottom: 0, right: 20)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ResultCollectionView {
+    private func makeCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        configuration.contentInsetsReference = .layoutMargins
+        
+        return UICollectionViewCompositionalLayout(sectionProvider: { sectionIndex, environment in
+            
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalWidth(1.2)
+                )
+            )
+            
+            let group = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)
+                ),
+                subitems: [item]
+            )
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = 10
+            section.orthogonalScrollingBehavior = .continuous
+            
+            return section
+        }, configuration: configuration)
+    }
+}
