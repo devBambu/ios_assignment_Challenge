4a1250aca2066822128a211bce78a00a01b48ea8
t2025-m0143 <>
2026-03-18 15:24:31 +0900

    ♻️refactor: MusicCollectionView Section, Item 타입 내재화

--- Assignment_Challenge/Assignment_Challenge/Model/NetworkService.swift
+++ Assignment_Challenge/Assignment_Challenge/Model/NetworkService.swift
@@ -29,7 +29,7 @@ final class NetworkService {
     }
     
     // 음악 정보
-    fileprivate func fetchMusic(of section: Section) async throws -> [Music] {
+    fileprivate func fetchMusic(of section: MusicCollectionView.Section) async throws -> [Music] {
         let query: (term: String, limit: Int) = switch section {
         case .spring:
             (term: "봄", limit: 5)
@@ -92,7 +92,7 @@ final class NetworkService {
 extension NetworkService: ReactiveCompatible { }
 
 extension Reactive where Base: NetworkService {
-    func fetchMusic(of section: Section) -> Single<[Music]> {
+    func fetchMusic(of section: MusicCollectionView.Section) -> Single<[Music]> {
         Single.create { [base] observer in
             let task = Task {
                 do {
